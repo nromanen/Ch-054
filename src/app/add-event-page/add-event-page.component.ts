f398179fc04d6d9b0feffa1830f4dc36bfe10ea0
okedo <okedo@mail.ru>
2017-09-26 01:28:50 +0300

    add event

---
+++ src/app/add-event-page/add-event-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-add-event-page',
+  templateUrl: './add-event-page.component.html',
+  styleUrls: ['./add-event-page.component.scss']
+})
+export class AddEventPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SpecificEventComponent } from './specific-event/specific-event.component';
 import { FooterComponent } from './footer/footer.component';
 import { AllEventsComponentComponent } from './all-events-component/all-events-component.component';
+import { AddEventPageComponent } from './add-event-page/add-event-page.component';
 
 
 @NgModule({
@@ -14,10 +17,12 @@ import { AllEventsComponentComponent } from './all-events-component/all-events-c
     NavBarComponent,
     SpecificEventComponent,
     FooterComponent,
-    AllEventsComponentComponent
+    AllEventsComponentComponent,
+    AddEventPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
