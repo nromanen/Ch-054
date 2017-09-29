6684106b20f89c2f85f1bded756d0489631cbfd3
nuta88 <anna.vagner-88@mail.ru>
2017-09-29 10:07:21 +0300

    Changed routes.

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -6,11 +6,11 @@ import { AddLectionPageComponent } from './add-lection-page/add-lection-page.com
 import { SpecificEventComponent } from './specific-event/specific-event.component';
 
 const routes: Routes = [
-    {path: '', redirectTo: 'all-event-page', pathMatch: 'full'},
     {path: 'all-event-page', component: AllEventsComponentComponent},
     {path: 'add-event-page', component: AddEventPageComponent},
     {path: 'add-lection-page', component: AddLectionPageComponent},
-    {path: 'specific-event-page', component: SpecificEventComponent }
+    {path: 'specific-event-page', component: SpecificEventComponent },
+    {path: '', redirectTo: 'specific-event-page', pathMatch: 'full'}
 ];
 
   @NgModule({
