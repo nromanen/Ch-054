6958c0a3e51e614d4ae232de3e69949f63a5aa6d
Ganna Vagner <gvagntc@training.local>
2017-10-05 11:45:17 +0300

    aa

--- src/app/specific-event/specific-event.component.ts
+++ src/app/specific-event/specific-event.component.ts
@@ -2,7 +2,7 @@ import { Component, HostListener, Inject, OnInit } from "@angular/core";
 import { DOCUMENT } from '@angular/platform-browser';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { Conf } from '../conf';
+import {Conf} from '../conf';
 import * as firebase from 'firebase';
 
 @Component({
@@ -11,50 +11,37 @@ import * as firebase from 'firebase';
   styleUrls: ['./specific-event.component.scss']
 })
 export class SpecificEventComponent implements OnInit {
-  background = { link: "/assets/images/1.png" }
-  bgLocation = { link: "/assets/images/bg-location.png" }
-  item = new Conf();
+  background={link:"/assets/images/1.png"}
+  bgLocation={link:"/assets/images/bg-location.png"}
+  items: Conf[] = [];
 
   public isShow: boolean = false;
+  
+  constructor(@Inject(DOCUMENT) private document: Document, private route: ActivatedRoute, private af: AngularFireDatabase) { }
 
-  constructor( @Inject(DOCUMENT) private document: Document, private route: ActivatedRoute, private af: AngularFireDatabase) { }
-
-
+ 
   goTo(location: string): void {
-    window.location.hash = '';
+    window.location.hash = ''; 
     window.location.hash = location;
-  }
+}
 
-  up(): any {
-    let t;
-    function onEdit() {
-      let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
-      if (top > 0) {
-        window.scrollBy(0, -100);
-        this.t = setTimeout(onEdit, 20);
+up():any{
+  let t;
+  function onEdit(){
+      let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+      if(top > 0) {
+        window.scrollBy(0,-100);
+        this.t = setTimeout(onEdit,20);
       } else clearTimeout(this.t);
       return false;
-    }
+    } 
     return onEdit();
   }
-  key: any;
+  key:any;
   ngOnInit(): void {
     this.key = this.route.snapshot.params['key'];
-    this.item.key = this.key;
-    this.af.list(`conference/${this.key}`, { preserveSnapshot: true })
-      .subscribe((snapshots) => {
-        snapshots.forEach(snapshot => {
-          this.item[snapshot.key] = snapshot.val();
-        });
-        this.item['confDateTo'] = new Date(this.item['confDateTo']['year'],this.item['confDateTo']['month'],this.item['confDateTo']['day'])
-    // console.log(this.item.confLections[3].speakerPhoto);
-       
-      }
-    );
-
-
-
-
+    var adaRef = firebase.database().ref("conference/" +this.key);
+    // console.log(adaRef.key);
   }
 
   @HostListener("window:scroll", [])
