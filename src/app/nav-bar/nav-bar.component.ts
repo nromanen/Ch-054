28ace0841e854658972cfbcf8252df006983f4b5
nuta88 <anna.vagner-88@mail.ru>
2017-10-08 02:16:46 +0300

    Formatted footer.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,8 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-  img_logo ="/assets/images/logo-pr.png";
-  background={link:"/assets/images/imgNav.jpg"}
+  imgLogo ='/assets/images/logo-pr.png';
   constructor() { }
 
   ngOnInit() {
