abadc68a2c1c14e8667e9028f1c5a2415a483dd2
Ganna Vagner <gvagntc@training.local>
2017-10-10 20:06:56 +0300

    Added autocomplete.

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -9,21 +9,20 @@ import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 	styleUrls: ['./add-event.component.scss']
 })
 export class AddEventComponent implements OnInit {
-	logoCamera = '/assets/images/camera.png';
+	logoCamera: string = '/assets/images/camera.png';
+	isShowCalendarTo: boolean = false;
 
-
+//autocomplete
 	model1 = "USA";
 	arrayOfStrings: string[] =
 	["Central Park West, New York, NY 10023, USA", "60 E 65th St, New York, NY 10065, USA", "Time Warner Center, 10 Columbus Cir, New York, NY 10023, USA", "900 Canada Pl, Vancouver, BC V6C 3L5, Canada", "101 Trans-Canada Hwy, Duncan, BC V9L 3P8, Canada", "999 Canada Pl #410, Vancouver, BC V6C 3E1, Canada",];
-
 
 	myCallback(newVal) {
 		this.model1 = newVal;
 	}
 
-
-	json(obj) {
-		return JSON.stringify(obj);
+	addCalendar() {
+		this.isShowCalendarTo;
 	}
 
 
