2702986ec9a07445f19d19841e09dff9d6a1ce3a
Ganna Vagner <gvagntc@training.local>
2017-10-18 20:12:27 +0300

    Changed calendar.

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -15,10 +15,11 @@ export class AddEventComponent implements OnInit {
 	logoCamera: string = '/assets/images/camera.png';
 	isShowCalendarTo: boolean = false;
 	isShowButton: boolean = true;
+	isShowIcon: boolean = false;
 	imgEvent: string = '';
 
 	//autocomplete
-	model1 = "USA";
+	model1 = "";
 	arrayOfStrings: string[] =
 	["Central Park West, New York, NY 10023, USA", "60 E 65th St, New York, NY 10065, USA", "Time Warner Center, 10 Columbus Cir, New York, NY 10023, USA", "900 Canada Pl, Vancouver, BC V6C 3L5, Canada", "101 Trans-Canada Hwy, Duncan, BC V9L 3P8, Canada", "999 Canada Pl #410, Vancouver, BC V6C 3E1, Canada",];
 	myForm: FormGroup;
@@ -30,12 +31,14 @@ export class AddEventComponent implements OnInit {
 	addCalendar() {
 		this.isShowCalendarTo = true;
 		this.isShowButton = false;
+		this.isShowIcon = true;
 		console.log(this.isShowCalendarTo);
 	}
 
 	deleteCalendar() {
 		this.isShowCalendarTo = false;
 		this.isShowButton = true;
+		this.isShowIcon = false;
 	}
 	constructor(private fb: FormBuilder) { }
 
